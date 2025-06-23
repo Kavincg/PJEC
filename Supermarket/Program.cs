ebe980fb223804612944e4a65e9005dc744b7635
Ukkadala Kirananjali <2406739@cognizant.com>
2025-06-23 11:58:29 +0530

    added main method in program.cs

--- Supermarket/Program.cs
+++ Supermarket/Program.cs
@@ -7,66 +7,77 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Supermarket.RolesAndEmailSender;
 using Stripe;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
-builder.Services.ConfigureApplicationCookie(options =>
-    {
-        options.LoginPath = $"/Identity/Account/Login";
-        options.LogoutPath = $"/Identity/Account/Logout";
-        options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
-    }
-);
-
-builder.Services.AddRazorPages();
-builder.Services.AddScoped<IEmailSender, EmailSender>();
-
-// Register DbInitializer
-builder.Services.AddScoped<IDbInitializer, DbInitializer>();
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Supermarket
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+            }
+            );
+
+            builder.Services.AddRazorPages();
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+            // Register DbInitializer
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapRazorPages();
+
+            // Initialize the database with roles and admin user
+            SeedDatabase();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+
+            app.Run();
+
+            // Helper method to seed the database
+            void SeedDatabase()
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                    dbInitializer.Initialize();
+                }
+            }
+        }
+
+    }
 }
 
-app.UseHttpsRedirection();
-StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapRazorPages();
-
-// Initialize the database with roles and admin user
-SeedDatabase();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
-
-// Helper method to seed the database
-void SeedDatabase()
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-        dbInitializer.Initialize();
-    }
-}
